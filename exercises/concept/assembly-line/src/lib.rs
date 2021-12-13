000e65af202ed4a52636557b6e0d2ac8f54db09e
Pham Duy Hoan <phamduyhoan95@gmail.com>
2021-12-13 16:21:48 +0700

    Add assembly-line

--- exercises/concept/assembly-line/src/lib.rs
+++ exercises/concept/assembly-line/src/lib.rs
@@ -1,11 +1,17 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
 #![allow(unused)]
-
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    // unimplemented!("calculate hourly production rate at speed: {}", speed)
+    const num_car:f64 = 221.0;
+    match(speed){
+        0 => 0.0,
+        1..=4 => speed as f64 * num_car,
+        5..=8 => speed as f64 * num_car*0.9,
+        9..=10 => speed as f64 * num_car*0.77,
+        _ => panic!("Un catch production_rate_per_hour")
+    }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    // unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed)/60.0) as u32
 }
