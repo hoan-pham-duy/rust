a71fa5b779d40b7ea91ceb1d312ff0f41550af91
Pham Duy Hoan <phamduyhoan95@gmail.com>
2021-12-13 16:24:30 +0700

    [NOT DONE ResistorColor ]

--- exercises/concept/resistor-color/src/lib.rs
+++ exercises/concept/resistor-color/src/lib.rs
@@ -1,28 +1,38 @@
-#[derive(Debug, PartialEq)]
+use enum_iterator::IntoEnumIterator;
+use int_enum::IntEnum;
+
+#[repr(u8)]
+#[derive(Clone, Copy, Eq, IntEnum, Debug, IntoEnumIterator, PartialEq)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black = 0,
+    Blue = 1,
+    Brown = 2,
+    Green = 3,
+    Grey = 4,
+    Orange = 5,
+    Red = 6,
+    Violet = 7,
+    White = 8,
+    Yellow = 9,
 }
 
 pub fn color_to_value(_color: ResistorColor) -> usize {
-    unimplemented!("convert a color into a numerical representation")
+    _color.int_value() as usize
 }
 
 pub fn value_to_color_string(value: usize) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    // unimplemented!(
+    //     "convert the value {} into a string representation of color",
+    //     value
+    // )
+    format!("{:#?}", ResistorColor::from_int(value.try_into().unwrap()))
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    unimplemented!("return a list of all the colors ordered by resistance")
+    // unimplemented!("return a list of all the colors ordered by resistance")
+    let mut vec = Vec::new();
+    for color in ResistorColor::into_enum_iter() {
+        vec.push(color);
+    }
+    vec
 }
