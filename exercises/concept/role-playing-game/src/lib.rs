69d403b49fcba14921637621cfbb6a1dd0d6d2ac
Pham Duy Hoan <phamduyhoan95@gmail.com>
2022-01-07 14:36:49 +0700

    Complete low-power-embedded-game

--- exercises/concept/role-playing-game/src/lib.rs
+++ exercises/concept/role-playing-game/src/lib.rs
@@ -10,10 +10,45 @@ pub struct Player {
 
 impl Player {
     pub fn revive(&self) -> Option<Player> {
-        unimplemented!("Revive this player")
+        return if self.health == 0 {
+            Some(Self {
+                health: 100,
+                mana: {
+                    if self.level >= 10 {
+                        Some(100)
+                    } else {
+                        None
+                    }
+                },
+                level: self.level
+            })
+        } else {
+            None
+        }
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        unimplemented!("Cast a spell of cost {}", mana_cost)
+        // unimplemented!("Cast a spell of cost {}", mana_cost)
+        let ret_val = match self.mana{
+            None => {
+                // if self.health >= mana_cost {
+                //     self.health -= mana_cost
+                // } else {
+                //     self.health = 0;
+                // }
+                // 0
+                self.health = self.health.saturating_sub(mana_cost);
+                0
+            }
+            Some(curr_mana) => {
+                return if curr_mana >= mana_cost {
+                    self.mana = Some(curr_mana - mana_cost);
+                    mana_cost * 2
+                } else {
+                    0
+                }
+            }
+        };
+        return ret_val;
     }
 }
