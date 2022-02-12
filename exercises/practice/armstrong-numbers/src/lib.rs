6c8a32b2ee46464167120714b815d7c939fef5e0
Pham Duy Hoan <phamduyhoan95@gmail.com>
2022-02-12 15:03:54 +0000

    Finish armstrong-numbers

--- exercises/practice/armstrong-numbers/src/lib.rs
+++ exercises/practice/armstrong-numbers/src/lib.rs
@@ -1,3 +1,9 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    // unimplemented!("true if {} is an armstrong number", num);
+    let num_digit_count = num.to_string().len();
+    let mut sum = 0;
+    for digit in num.to_string().chars() {
+        sum += digit.to_digit(10).unwrap().pow(num_digit_count as u32);
+    }
+    sum == num
 }
