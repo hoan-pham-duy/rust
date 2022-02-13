3eb215c416f0b8d321bf8d225c674a00e9f9e155
Pham Duy Hoan <phamduyhoan95@gmail.com>
2022-02-13 08:19:14 +0000

    Complete prime-factor

--- exercises/practice/prime-factors/src/lib.rs
+++ exercises/practice/prime-factors/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    // unimplemented!("This should calculate the prime factors of {}", n);
+    //Primes factors of number
+    let mut num = n;
+    let mut factors = Vec::new();
+    let mut factor = 2;
+    while num > 1 {
+        if num % factor == 0 {
+            factors.push(factor);
+            num /= factor;
+        } else {
+            factor += 1;
+        }
+    }
+    factors
 }
