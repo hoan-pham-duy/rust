4c17630d2988bff92623d74f0bc93ebb95d245a5
Pham Duy Hoan <phamduyhoan95@gmail.com>
2022-02-13 04:09:52 +0000

    Complete difference-of-squares and grains

--- exercises/practice/difference-of-squares/src/lib.rs
+++ exercises/practice/difference-of-squares/src/lib.rs
@@ -1,14 +1,25 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{}", n)
+    //unimplemented!("square of sum of 1...{}", n)
+    let mut sum:u32 = 0;
+    for i in 1..n+1 {
+        sum += i;
+    }
+    sum.pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{}", n)
+    // unimplemented!("sum of squares of 1...{}", n)
+    let mut sum_of_squares: u32 = 0;
+    for i in 1..n+1 {
+        sum_of_squares += i.pow(2);
+    }
+    sum_of_squares
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!(
-        "difference between square of sum of 1...{n} and sum of squares of 1...{n}",
-        n = n,
-    )
+    // unimplemented!(
+    //     "difference between square of sum of 1...{n} and sum of squares of 1...{n}",
+    //     n = n,
+    // )
+    square_of_sum(n) - sum_of_squares(n)
 }
--- exercises/practice/grains/src/lib.rs
+++ exercises/practice/grains/src/lib.rs
@@ -1,7 +1,24 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    //unimplemented!("grains of rice on square {}", s);
+
+    let result:u64 = match s {
+        n if n<1 || n>64 => panic!("Square must be between 1 and 64"),
+        n => {
+            let mut result: u64 = 1;
+            for _ in 1..n {
+                result *= 2;
+            }
+            result
+        }  
+    };
+    result as u64
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    const NUM_GRAIN:u8 = 64;
+    let mut result: u64 = 0;
+    for i in 1..=NUM_GRAIN {
+        result += square(i as u32);
+    }
+    result
 }
