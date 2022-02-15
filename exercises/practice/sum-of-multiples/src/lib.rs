bfda293d7a586e953b97ffe922b6f452a9ab49a8
Pham Duy Hoan <phamduyhoan95@gmail.com>
2022-02-15 11:41:13 +0000

    Complete sum-of-multiples

--- exercises/practice/sum-of-multiples/src/lib.rs
+++ exercises/practice/sum-of-multiples/src/lib.rs
@@ -1,7 +1,22 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    // unimplemented!(
+    //     "Sum the multiples of all of {:?} which are less than {}",
+    //     factors,
+    //     limit
+    // )
+    let mut sum = 0;
+    for num_increase in 1..limit {
+        for factor in factors {
+            match factor {
+                0 => (),
+                _ => {
+                    if num_increase % factor == 0 {
+                        sum += num_increase;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    sum
 }
