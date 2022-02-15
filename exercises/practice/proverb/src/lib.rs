46efdbecea838d32903296011d86010cfc8a598e
Pham Duy Hoan <phamduyhoan95@gmail.com>
2022-02-15 11:25:42 +0000

    Complete raindrops

--- exercises/practice/proverb/src/lib.rs
+++ exercises/practice/proverb/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    // unimplemented!("build a proverb from this list of items: {:?}", list)
+    let mut result = String::new();
+    for i in 0..list.len() {
+        if i == list.len() - 1 {
+            result.push_str(&format!("And all for the want of a {}.", list[0]));
+        } else {
+            result.push_str(&format!(
+                "For want of a {} the {} was lost.\n",
+                list[i],
+                list[i + 1]
+            ));
+        }
+    }
+    result
 }
--- exercises/practice/raindrops/src/lib.rs
+++ exercises/practice/raindrops/src/lib.rs
@@ -1,3 +1,23 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    //unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut result = String::new();
+    for i in &[3, 5, 7] {
+        let n_mod_i = n%i;
+        match n_mod_i {
+            0 => {
+                match i {
+                    3 => result.push_str("Pling"),
+                    5 => result.push_str("Plang"),
+                    7 => result.push_str("Plong"),
+                    _ => {}
+                }
+            }
+            _ => {}
+        };
+    }
+
+    if result.is_empty() {
+        result = n.to_string();
+    }
+    return result;
 }
